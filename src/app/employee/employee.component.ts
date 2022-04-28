f1dd5a46a892b0b110c42afbad3d8cbeff911d71
muneeramm <muneeraabbas22@gmail.com>
2022-04-29 00:07:19 +0530

    initialcrud

---
+++ src/app/employee/employee.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms'
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent implements OnInit {
+  formValue !:FormGroup
+  constructor(private formbuilder:FormBuilder) { }
+
+  ngOnInit(): void {
+    this.formValue=this.formbuilder.group({
+      firstname:[''],
+      lastname:[''],
+      email:[''],
+      phoneno:[''],
+      salary:['']
+    })
+  }
+
+}
